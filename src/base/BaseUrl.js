e9c1d32c171c2dd893cf6eaafc623549c0940e34
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-09-22 03:46:39 -0700

    v3

--- src/base/BaseUrl.js
+++ src/base/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api"
+// export const BASE_URL = "http://localhost:8080/api"
+export const BASE_URL = "https://one-milion-course-server-b9404f6d4bf4.herokuapp.com/api"
