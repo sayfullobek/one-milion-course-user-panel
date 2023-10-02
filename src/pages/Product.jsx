9afac05fda49a9d63947297b0c080a0ba8287579
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-10-02 03:15:37 -0700

    bbsdas

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -78,11 +78,11 @@ export const Product = () => {
                                     <hr className="my-0"/>
                                     <div className="card-body">
                                         <div className="d-flex justify-content-between align-items-center pb-2 mb-1">
-                                            <button type="button" className="btn btn-primary"
+                                            <button type="button" className="btn"
                                                     onClick={() => basketAndLike('like')}><i
                                                 className="bi bi-suit-heart"/>
                                             </button>
-                                            <button type="button" className="btn btn-primary"
+                                            <button type="button" className="btn btn-success"
                                                     onClick={() => basketAndLike('basket')}><i
                                                 className="bi bi-cart4"/></button>
                                             <button type="button" className="btn btn-primary">Buy now</button>
